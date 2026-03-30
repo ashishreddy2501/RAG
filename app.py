a576e4df99526e7208f8b3cb8695ff49b8c635bb
ashishreddy2501 <ashishreddy2501@gmail.com>
2026-03-30 13:08:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import tempfile
 import os
 
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain.chains import RetrievalQA
